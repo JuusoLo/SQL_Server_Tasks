a2d6edfac63aeaf6951c181279bedbce7cbc0bea
JuusoLo <lonka.juuso@gmail.com>
2018-03-22 10:22:46 +0200

    SQL_Server update

---
+++ SQL_Server_Tuntitehtävä_1/SQL_Server_Tuntitehtävä_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SQL_Server_Tuntitehtävä_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
